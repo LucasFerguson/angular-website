58a475c0be9406ad1f3720ca1670d27da05da192
Lucas <lucasferguson16@gmail.com>
2020-04-23 22:39:10 -0500

    Game Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { GameComponent } from './game/game.component';
 
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { GameComponent } from './game/game.component';
 
 @NgModule({
 	declarations: [AppComponent, HomeComponent, ListComponent, GameComponent],
